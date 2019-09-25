2f37d4951b540afd513654b65d67619eb7d930bd
Apurv Patel <apurva19iosDev@gmail.com>
2019-09-25 10:41:12 -0500

    Added dummy data for search screen - working on styling now

---
+++ data/dummy-data.js
@@ -0,0 +1,44 @@
+import Search from '../models/search';
+
+export const SEARCH_DATA = [
+  new Search(
+    11598639,
+    'Lawn Mower',
+    'https://vader-prod.s3.amazonaws.com/1555007291-honda-self-propelled-lawn-mowers-hrx217hza-64-1000-1555007261.jpg',
+    'Apurva Patel',
+    20,
+    '1 day',
+  ),
+  new Search(
+    11598640,
+    '100ft Ladder',
+    'https://images.homedepot-static.com/productImages/35b45de4-96ee-4d9c-8f43-f972f89500a8/svn/werner-extension-ladders-d1216-2-64_1000.jpg',
+    'Jonathan Harris',
+    15,
+    '1 day',
+  ),
+  new Search(
+    11598641,
+    'Vaccum',
+    'https://secure.img1-fg.wfcdn.com/im/28715865/compr-r85/7120/71207075/hoover-total-home-pet-bagless-upright-vacuum.jpg',
+    'Bob Builder',
+    23,
+    '1 day',
+  ),
+  new Search(
+    11598642,
+    'Air Blower',
+    'https://rukminim1.flixcart.com/image/832/832/blower/h/5/n/spt500-stanley-original-imaejfe5ksbzkdqn.jpeg?q=70',
+    'Tommy John',
+    18,
+    '1 day',
+  ),
+  new Search(
+    11598643,
+    'BBQ Grill',
+    'https://images.homedepot-static.com/productImages/d61ed935-1441-4aa8-b47f-42d67dd3060a/svn/royal-gourmet-cart-style-grills-cd1824a-64_1000.jpg',
+    'Shaily Cost',
+    19,
+    '1 day',
+  ),
+];
