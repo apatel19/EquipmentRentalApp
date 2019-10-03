77597d108aa3e122512882b4d84de56e4f2e166a
Apurv Patel <apurva19iosDev@gmail.com>
2019-10-03 14:04:15 -0500

    Adding dummy data for search screen (#10)
    * Added dummy data for search screen - working on styling now
    
    * Added dummy data for search screen - working on styling now

---
+++ models/search.js
@@ -0,0 +1,12 @@
+class Search {
+  constructor(id, itemName, imageUrl, ownerName, price, time) {
+    this.id = id;
+    this.itemName = itemName;
+    this.imageUrl = imageUrl;
+    this.ownerName = ownerName;
+    this.price = price;
+    this.time = time;
+  }
+}
+
+export default Search;
--- screens/SearchScreenContainer.js
+++ screens/SearchScreenContainer.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList, ImageBackground} from 'react-native';
+import {SEARCH_DATA} from '../data/dummy-data';
 
 const SearchScreenContainer = props => {
   return (
-    <View style={styles.screen}>
-      <Text>This is search screen!</Text>
+    <View>
+      <FlatList
+        data={SEARCH_DATA}
+        keyExtractor={(item, index) => item.id}
+        renderItem={itemData => (
+          <View style={styles.card}>
+            <View>
+              <Text>{itemData.item.itemName}</Text>
+            </View>
+            <View style={styles.imageContainer}>
+              <ImageBackground
+                style={styles.image}
+                source={{uri: itemData.item.imageUrl}}
+              />
+            </View>
+            <View style={styles.discription}>
+              <Text>{itemData.item.price}</Text>
+              <Text>{itemData.item.time}</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 };
@@ -16,11 +37,22 @@ SearchScreenContainer.navigationOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  card: {
+    flexDirection: 'row',
+    height: 100,
+    width: '100%',
+    padding: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    elevation: 3,
   },
+  imageContainer: {},
+  image: {
+    width: 100,
+    height: 100,
+  },
+  discription: {},
 });
 
 export default SearchScreenContainer;
