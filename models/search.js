8c2d1ef46eceebfbfd1679a0868f1e7a2f883870
Apurv Patel <apurva19iosDev@gmail.com>
2019-10-12 12:03:13 -0500

    Added more styling for search screen view and seperated components

--- models/search.js
+++ models/search.js
@@ -1,7 +1,7 @@
 class Search {
-  constructor(id, itemName, imageUrl, ownerName, price, time) {
+  constructor(id, title, imageUrl, ownerName, price, time) {
     this.id = id;
-    this.itemName = itemName;
+    this.title = title;
     this.imageUrl = imageUrl;
     this.ownerName = ownerName;
     this.price = price;
--- screens/SearchScreenContainer.js
+++ screens/SearchScreenContainer.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList, ImageBackground} from 'react-native';
+import {FlatList, Button} from 'react-native';
 import {SEARCH_DATA} from '../data/dummy-data';
+import ProductItem from '../components/UI/ProductItem';
+import Colors from '../constants/Colors';
 
 const SearchScreenContainer = props => {
   return (
-    <View>
-      <FlatList
-        data={SEARCH_DATA}
-        keyExtractor={(item, index) => item.id}
-        renderItem={itemData => (
-          <View style={styles.card}>
-            <View>
-              <Text>{itemData.item.itemName}</Text>
-            </View>
-            <View style={styles.imageContainer}>
-              <ImageBackground
-                style={styles.image}
-                source={{uri: itemData.item.imageUrl}}
-              />
-            </View>
-            <View style={styles.discription}>
-              <Text>{itemData.item.price}</Text>
-              <Text>{itemData.item.time}</Text>
-            </View>
-          </View>
-        )}
-      />
-    </View>
+    <FlatList
+      data={SEARCH_DATA}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          image={itemData.item.imageUrl}
+          onSelect={() => {}}>
+          <Button
+            color={Colors.primary}
+            title="More Info."
+            onPress={() => {}}
+          />
+          <Button
+            color={Colors.primary}
+            title="Add to Cart"
+            onPress={() => {}}
+          />
+        </ProductItem>
+      )}
+    />
   );
 };
 
@@ -35,24 +38,5 @@ SearchScreenContainer.navigationOptions = navData => {
     headerTitle: 'Search',
   };
 };
-
-const styles = StyleSheet.create({
-  card: {
-    flexDirection: 'row',
-    height: 100,
-    width: '100%',
-    padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: {width: 0, height: 2},
-    elevation: 3,
-  },
-  imageContainer: {},
-  image: {
-    width: 100,
-    height: 100,
-  },
-  discription: {},
-});
 
 export default SearchScreenContainer;
