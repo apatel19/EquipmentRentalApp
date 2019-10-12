e873b77a531c2fe2cfbb9073d3181f7bae2fb4b1
Apurv Patel <apurva19iosDev@gmail.com>
2019-10-12 12:03:13 -0500

    Added more styling for search screen view and seperated components

---
+++ components/UI/ProductItem.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
+import Card from '../UI/Card';
+
+const ProductItem = props => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
+  return (
+    <Card style={styles.product}>
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onSelect} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{uri: props.image}} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <View style={styles.priceContainer}>
+                <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+                <Text style={styles.perDay}>/ Day</Text>
+              </View>
+            </View>
+            <View style={styles.action}>{props.children}</View>
+          </View>
+        </TouchableCmp>
+      </View>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  product: {
+    height: 300,
+    margin: 20,
+  },
+  touchable: {
+    overflow: 'hidden',
+    borderRadius: 10,
+  },
+  imageContainer: {
+    width: '100%',
+    height: '60%',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 17,
+    marginVertical: 2,
+  },
+  priceContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  perDay: {
+    fontSize: 14,
+    color: '#888',
+    paddingHorizontal: 5,
+  },
+  price: {
+    fontSize: 14,
+    color: '#888',
+  },
+  action: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '23%',
+    paddingHorizontal: 20,
+  },
+  details: {
+    alignItems: 'center',
+    height: '17%',
+    padding: 10,
+  },
+});
+
+export default ProductItem;
