a09710983ac1bfe7f42f0f65e9a51eab3a8384ca
Lauren Kirkley <lfkirkley@crimson.ua.edu>
2019-10-17 11:17:58 -0500

    Creating pr.

--- screens/OrdersHistoryScreenContainer.js
+++ screens/OrdersHistoryScreenContainer.js
@@ -2,25 +2,25 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 const OrdersHistoryScreenContainer = props => {
-    return (
-      <View style={styles.screen}>
-        <Text>order history</Text>
-      </View>
-    );
-  };
+  return (
+    <View style={styles.screen}>
+      <Text>order history - screen</Text>
+    </View>
+  );
+};
 
-  OrdersHistoryScreenContainer.navigationOptions = navData => {
-    return {
-      headerTitle: 'Search',
-    };
+OrdersHistoryScreenContainer.navigationOptions = navData => {
+  return {
+    headerTitle: 'Search',
   };
+};
 
-  const styles = StyleSheet.create({
-    screen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
-  
-  export default OrdersHistoryScreenContainer;
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default OrdersHistoryScreenContainer;
