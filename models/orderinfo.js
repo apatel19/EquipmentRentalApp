03ff4372d9a3ef39a93367ef801fac57b6d5f8ce
Lauren Kirkley <lfkirkley@crimson.ua.edu>
2019-11-05 11:21:18 -0600

    updates

---
+++ models/orderinfo.js
@@ -0,0 +1,11 @@
+class OrderInfo {
+  constructor(orderNumber, name, imageUrl, price, status) {
+    this.orderNumber = orderNumber;
+    this.name = name;
+    this.imageUrl = imageUrl;
+    this.price = price;
+    this.status = status;
+  }
+}
+
+export default OrderInfo;
