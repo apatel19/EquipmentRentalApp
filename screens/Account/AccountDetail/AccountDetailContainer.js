e69ee76d9b45824855f7828ab5ca3acf7dfd51f5
Jameson Evans <6jh7ln@gmail.com>
2019-11-14 11:23:12 -0600

    added name, email, pw change fields

--- screens/Account/AccountDetail/AccountDetailContainer.js
+++ screens/Account/AccountDetail/AccountDetailContainer.js
@@ -1,14 +1,62 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 const AccountDetailContainer = props => {
   return (
-    <View>
-      <Text>Account Detail</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior="padding"
+      keyboardVerticalOffset={100}>
+      <ScrollView>
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.formLabel}>Name</Text>
+            <TextInput style={styles.input} title="Change Name" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.formLabel}>Email</Text>
+            <TextInput style={styles.input} title="Change Email" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.formLabel}>Password</Text>
+            <TextInput style={styles.input} title="Change Password" />
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <Button title="Save" onPress={() => {}} />
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  formContainer: {
+    margin: 20,
+  },
+  inputContainer: {},
+  formLabel: {
+    fontSize: 16,
+    marginVertical: 8,
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  buttonContainer: {},
+});
 
 export default AccountDetailContainer;
