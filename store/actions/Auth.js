265460399da32f80b49d185f7f3e0a9f8c9a4c7a
Apurv Patel <apurva19iosDev@gmail.com>
2019-11-21 14:22:55 -0600

    I hid the API keys

--- store/actions/Auth.js
+++ store/actions/Auth.js
@@ -1,10 +1,14 @@
+import {REACT_NATIVE_APP_GOOGLE_API_KEY} from 'react-native-dotenv';
+
 export const SIGNUP = 'SIGNUP';
 export const LOGIN = 'LOGIN';
 
 export const signup = (email, password, name) => {
   return async dispatch => {
+    console.log('API:', REACT_NATIVE_APP_GOOGLE_API_KEY);
+
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDQeyuI-UBA9Q7a4YgZWCT0k_Gfok0S460',
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${REACT_NATIVE_APP_GOOGLE_API_KEY}`,
       {
         method: 'POST',
         headers: {
